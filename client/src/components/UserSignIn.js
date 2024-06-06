80ebe41e9f39ce876cba83767b40f101253fa14a
saturn229 <makroyd17@gmail.com>
2024-06-05 20:53:08 -0400

    updated sign in pages

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,3 +1,6 @@
+import { useContext, useRef, useState } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+
 const UserSignIn = () => {
     /*
     This component provides the "Sign In" screen by rendering a form 
@@ -5,4 +8,45 @@ const UserSignIn = () => {
     The component also renders a "Sign In" button that when clicked signs in the user 
     and a "Cancel" button that returns the user to the default route (i.e. the list of courses).
     */
+
+
+    // State
+    email = useRef(null);
+    password = useRef(null);
+    const [errors, setErrors] = useState([]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+      }
+
+    const handleCancel = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <div className="form--centered">
+            <h2>Sign In</h2>
+            <form>
+                <label htmlFor="emailAddress">Email Address</label>
+                <input id="emailAddress" name="emailAddress" type="email" defaultValue="" />
+                <label htmlFor="password">Password</label>
+                <input id="password" name="password" type="password" defaultValue="" />
+                <button className="button" type="submit">
+                    Sign In
+                </button>
+                <button
+                    className="button button-secondary"
+                    onclick="event.preventDefault(); location.href='index.html';"
+                >
+                    Cancel
+                </button>
+            </form>
+            <p>
+                Don't have a user account? Click here to <a href="sign-up.html">sign up</a>!
+            </p>
+        </div>
+
+    )
 }
+
+export default UserSignIn;
--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,8 +1,79 @@
-const UserSignUp = () => {
+import { useContext, useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const UserSignIn = () => {
     /*
         This component provides the "Sign Up" screen by rendering a form that allows a 
         user to sign up by creating a new account. The component also renders a "Sign Up" 
         button that when clicked sends a POST request to the REST API's /api/users route and signs in the user. 
         This component also renders a "Cancel" button that returns the user to the default route (i.e. the list of courses).
     */
+
+
+    // State
+    const firstname = useRef(null);
+    const lastname = useRef(null);
+    const email = useRef(null);
+    const password = useRef(null);
+    const [errors, setErrors] = useState([]);
+
+
+    // event handlers
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    const handleCancel = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <div className="form--centered">
+            <h2>Sign Up</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="firstName">First Name</label>
+                <input
+                    id="firstName"
+                    name="firstName"
+                    type="text"
+                    defaultValue=""
+                />
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                    id="lastName"
+                    name="lastName"
+                    type="text"
+                    defaultValue=""
+                />
+                <label htmlFor="emailAddress">Email Address</label>
+                <input
+                    id="emailAddress"
+                    name="emailAddress"
+                    type="email"
+                    defaultValue=""
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    defaultValue="" />
+                <button className="button" type="submit">
+                    Sign Up
+                </button>
+                <button
+                    className="button button-secondary"
+                    onclick="event.preventDefault(); location.href='index.html';"
+                >
+                    Cancel
+                </button>
+            </form>
+            <p>
+                Already have a user account? Click here to{" "}
+                <a href="sign-in.html">sign in</a>!
+            </p>
+        </div>
+    )
 }
+
+export default UserSignIn;
